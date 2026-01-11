7eba4e8aa6f02a8c00fdec572f70be42b36b4c3b
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:47:33 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_identity_provider_twitters" {
+  description = "All api_management_identity_provider_twitter resources"
+  value       = azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters
+}
+

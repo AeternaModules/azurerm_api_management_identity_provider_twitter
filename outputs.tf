5fb48736b0d9a0336d0641fdbc3d7cb54978e7b1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,19 @@
-output "api_management_identity_provider_twitters" {
-  description = "All api_management_identity_provider_twitter resources"
-  value       = azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters
-  sensitive   = true
-}
 output "api_management_identity_provider_twitters_api_key" {
-  description = "List of api_key values across all api_management_identity_provider_twitters"
-  value       = [for k, v in azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters : v.api_key]
+  description = "Map of api_key values across all api_management_identity_provider_twitters, keyed the same as var.api_management_identity_provider_twitters"
+  value       = { for k, v in azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters : k => v.api_key }
   sensitive   = true
 }
 output "api_management_identity_provider_twitters_api_management_name" {
-  description = "List of api_management_name values across all api_management_identity_provider_twitters"
-  value       = [for k, v in azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_identity_provider_twitters, keyed the same as var.api_management_identity_provider_twitters"
+  value       = { for k, v in azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters : k => v.api_management_name }
 }
 output "api_management_identity_provider_twitters_api_secret_key" {
-  description = "List of api_secret_key values across all api_management_identity_provider_twitters"
-  value       = [for k, v in azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters : v.api_secret_key]
+  description = "Map of api_secret_key values across all api_management_identity_provider_twitters, keyed the same as var.api_management_identity_provider_twitters"
+  value       = { for k, v in azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters : k => v.api_secret_key }
   sensitive   = true
 }
 output "api_management_identity_provider_twitters_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_identity_provider_twitters"
-  value       = [for k, v in azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_identity_provider_twitters, keyed the same as var.api_management_identity_provider_twitters"
+  value       = { for k, v in azurerm_api_management_identity_provider_twitter.api_management_identity_provider_twitters : k => v.resource_group_name }
 }
 
